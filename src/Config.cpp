2186d2b0685dfd92f1611dc396aa2016730340a0
aglab2 <aglab3@gmail.com>
2026-07-27 22:22:11 +0800

    Revert "Disable direct composition for now"
    This reverts commit 3fc06ef5366ac031a3a47ae0b6f240d01bea8ade.

--- src/Config.cpp
+++ src/Config.cpp
@@ -215,8 +215,6 @@ static bool isWine(void)
 
 void Config::validate()
 {
-	config.angle.directComposition = false;
-
 	// Only OpenGL is capable of FB emulation
 	if (frameBufferEmulation.enable != 0)
 	{
