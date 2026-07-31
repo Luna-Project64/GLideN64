cbe86735d8d90c3eee53cefedf39ea22c5a7fdfb
aglab2 <aglab3@gmail.com>
2026-07-31 09:26:53 +0800

    Revert "Use SSE4.2"
    This reverts commit cc7f068f0dd837176027f89530162bba098e835a.

--- src/windows/ZilmarAPIImpl_windows.cpp
+++ src/windows/ZilmarAPIImpl_windows.cpp
@@ -44,7 +44,7 @@ void PluginAPI::GetDllInfo(PLUGIN_INFO * PluginInfo)
 {
 	PluginInfo->Version = 0x103;
 	PluginInfo->Type = PLUGIN_TYPE_GFX;
-	sprintf(PluginInfo->Name, "ANGLE %s v4.3.37", pluginName);
+	sprintf(PluginInfo->Name, "ANGLE %s v4.3.37hf", pluginName);
 	PluginInfo->NormalMemory = FALSE;
 	PluginInfo->MemoryBswaped = TRUE;
 }
